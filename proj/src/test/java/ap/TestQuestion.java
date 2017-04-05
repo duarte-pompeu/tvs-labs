7443971d6d9f3b87a196061c27d9a2cce95adbc6
Leomark7 <leomark7@gmail.com>
2017-04-05 18:50:23 +0100

    Implemented test case 1.

--- proj/src/test/java/ap/TestQuestion.java
+++ proj/src/test/java/ap/TestQuestion.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 import ap.Question;
+import ap.exception.*;
 
 public class TestQuestion {
 	
@@ -9,12 +11,26 @@ public class TestQuestion {
 		List<String> choices = new ArrayList<String>();
 		choices.add("Luis de Camoes");
 		choices.add("Fernando Pessoa");
+
 		Question question = new Question("Quem escreveu os Lusiadas", choices, 1, "Portugues", 20);
+
+		assertEquals(question.getSubjects().get(0), "Portugues");
+		assertEquals(question.getSubjects().size(), 1);
+		assertEquals(question.getWeight(), 20);
+		assertEquals(question.grade(1), 20);
+		assertEquals(question.grade(2), -20);
 	}
 
 	@Test
 	public void choiceLengthOffMinBoundary() {
+		List<String> choices = new ArrayList<String>();
+		choices.add("Luis de Camoes");
+		choices.add("Fernando Pessoa");
 
+		/*try {
+			Question question = new Question("Quem escreveu os Lusiadas", choices, 1, "Portugues", 20);
+			fail();
+		} catch (InvalidOperationException e)*/
 	}
 
 	@Test
