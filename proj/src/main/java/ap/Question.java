1aaaf1cb7c71db921f626ccc8e6fe09d3d9e32f6
Leomark7 <leomark7@gmail.com>
2017-04-05 19:58:05 +0100

    Implemented class Question.

--- proj/src/main/java/ap/Question.java
+++ proj/src/main/java/ap/Question.java
@@ -4,25 +4,75 @@ import java.util.*;
 import ap.exception.*;
 
 public  class Question{
-	public Question(String body, List<String> choices, int correctChoice, 
-	String subject, int weight)
-	{}
+	private String body;
+	private List<String> choices;
+	private int correctChoice;
+	private List<String> subjects;
+	private int weight;
+
+
+	public Question(String body, List<String> choices, int correctChoice, String subject, int weight) throws InvalidOperationException {
+		this.body = body;
+
+		if (choices.size() >= 2 && choices.size() <= 10) {
+			this.choices = choices;
+		} else {
+			throw new InvalidOperationException();
+		}
+		
+		if (correctChoice > 0 && correctChoice <= choices.size()) {
+			this.correctChoice = correctChoice;
+		} else {
+			throw new InvalidOperationException();
+		}
+
+		this.subjects = new ArrayList<String>();
+		this.subjects.add(subject);
+
+		if (weight > 0 && weight <= 20) {
+			this.weight = weight;
+		} else {
+			throw new InvalidOperationException();
+		}
+	}
 	
-	public void remove(String subject) throws InvalidOperationException
-	{}
+	public void remove(String subject) throws InvalidOperationException	{
+		if (this.subjects.size() > 1) {
+			this.subjects.remove(subject);
+		} else {
+			throw new InvalidOperationException();
+		}
+	}
 	
-	public void add(String subject) throws InvalidOperationException
-	{}
+	public void add(String subject) throws InvalidOperationException {
+		if (this.subjects.size() < 3) {
+			this.subjects.add(subject);
+		} else {
+			throw new InvalidOperationException();
+		}
+	}
 	
-	public List<String> getSubjects()
-	{ return null;}
+	public List<String> getSubjects() {
+		return this.subjects;
+	}
 	
-	public float grade(int selectedChoice)
-	{ return 0;}
+	public float grade(int selectedChoice) {
+		if (selectedChoice == this.correctChoice) {
+			return this.weight;
+		} else {
+			return -(weight / (choices.size() - 1));
+		}
+	}
 	
-	public void setWeight(int weight) throws InvalidOperationException
-	{}
+	public void setWeight(int weight) throws InvalidOperationException {
+		if (weight > 0 && weight <= 20) {
+			this.weight = weight;
+		} else {
+			throw new InvalidOperationException();
+		}
+	}
 	
-	public int getWeight()
-	{ return 0; }
+	public int getWeight() {
+		return this.weight;
+	}
 }
